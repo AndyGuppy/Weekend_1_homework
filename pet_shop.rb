a79fe6e22bfaa31e87534a09d85fefdd6563ef14
andrew guppy <andrew.guppy@blueyonder.co.uk>
2016-11-25 12:12:47 +0000

    Added total cash functions

--- pet_shop.rb
+++ pet_shop.rb
@@ -5,3 +5,7 @@ def pet_shop_name(pet_shop)
 return pet_shop[:name]
 end
 
+# Test 2 rquires the total cash which should be 1000
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
