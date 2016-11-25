4a6407bf371d57f2cfb7266401528032a4a03f89
andrew guppy <andrew.guppy@blueyonder.co.uk>
2016-11-25 13:10:38 +0000

    created function to return the stock count

--- pet_shop.rb
+++ pet_shop.rb
@@ -12,13 +12,16 @@ end
 
 # Test 3 -- call the total_cash function and add the value passed to it, tests looks for 1010 for add and 990 for remove
 def add_or_remove_cash(pet_shop,cash)
-  pet_shop[:admin][:total_cash] = (cash + total_cash(pet_shop))
-  return total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash] +=cash
 end
 
 # Test 4 -- details of the pets sold
 def pets_sold(pet_shop)
     return pet_shop[:admin][:pets_sold]
+end
+
+def stock_count(pet_shop)
+    return pet_shop[:pets].count
 end
 
 
