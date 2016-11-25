f0d2f7fc08a87b61d7259581757536a2457fa6f3
andrew guppy <andrew.guppy@blueyonder.co.uk>
2016-11-25 12:33:13 +0000

    created function for adding or removing cash

--- pet_shop.rb
+++ pet_shop.rb
@@ -9,3 +9,10 @@ end
 def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
+
+# Test 3 -- call the total_cash function and add the value passed to it
+def add_or_remove_cash(pet_shop,cash)
+  pet_shop[:admin][:total_cash] = (cash + total_cash(pet_shop))
+  return total_cash(pet_shop)
+
+end
